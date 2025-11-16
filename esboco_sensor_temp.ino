02b564851ad46934ac661c66ca74cfb6e31c92b8
Maria Luisa <malulu1604@gmail.com>
2025-11-16 14:50:56 -0300

    first commit

---
+++ esboco_sensor_temp.ino
@@ -0,0 +1,35 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 4  // pino conectado ao DATA
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  Serial.println("Inicializando DS18B20...");
+
+  if (sensors.getDeviceCount() == 0) {
+    Serial.println("Nenhum sensor encontrado!");
+  } else {
+    Serial.print("Sensores encontrados: ");
+    Serial.println(sensors.getDeviceCount());
+  }
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
+  if (tempC == DEVICE_DISCONNECTED_C) {
+    Serial.println("Erro: DS18B20 desconectado!");
+  } else {
+    Serial.print("Temperatura: ");
+    Serial.print(tempC);
+    Serial.println(" °C");
+  }
+
+  delay(1000);
+}
